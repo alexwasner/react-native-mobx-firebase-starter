df5f6a9aff4ff2ecd9712fc284d5de3316bc0077
Alex Wasner <alex.wasner@gmail.com>
2019-01-15 15:39:54 -0600

    remove legacy code

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,12 +1,6 @@
 import firebase from 'react-native-firebase'
-import userStore from '../stores/UserStore'
 
 export default {
-  fetchUserSubscriptions:()=>{
-    let colRef = firebase.firestore().collection('users').doc(userStore.userId).collection('subscriptions');
-    
-  },
-
   createUserWithEmailAndPassword: (username, password) => firebase.auth().createUserWithEmailAndPassword(username, password),
   signInWithEmailAndPassword: (username, password) => firebase.auth().signInWithEmailAndPassword(username, password)
 }
