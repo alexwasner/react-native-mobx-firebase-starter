66bfc8c7618de6547d16fd3e4fa60c16f718cec3
Alex Wasner <alex.wasner@gmail.com>
2019-07-21 09:09:49 -0500

    Add LoaderView

---
+++ src/components/theme/LoaderView.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import {
+  Animated,
+  Easing,
+  View
+} from 'react-native'
+import { Icons } from '../../styles'
+import Animation from 'lottie-react-native';
+import LOADER_JSON from '../../assets/lottie/loading-circle.json'
+import styles from '../../depstyles/theme/LoaderViewStyles'
+
+export default class LoaderView extends Component {
+  constructor(props) {
+    super(props)
+    this.animation = new Animated.Value(0)
+  }
+
+  spin() {
+    Animated.loop(
+      Animated.timing(this.animation, {
+        toValue: 1,
+        duration: 1250,
+        easing: Easing.linear,
+        useNativeDriver: true
+      })
+    ).start()
+  }
+
+  componentDidMount() {
+    this.spin()
+  }
+
+  render() {
+    return (<View style={styles.loadingContainer}>
+      <Animation
+        style={[styles.loadingContainer, this.props.style]}
+        loop={true}
+        source={LOADER_JSON}
+        progress={this.animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+          extrapolate: 'clamp'
+        })}
+      />
+    </View>)
+  }
+
+
+  showAnimatedSpinner(style) {
+    const spin = this.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '359deg']
+    })
+    return (<Animated.Image
+      style={[style, { transform: [{ rotate: spin }] }]}
+      source={this.props.source || Icons.spinner}
+    />)
+  }
+
+}
